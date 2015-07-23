85dcf372e07c27a7eff3098f716867fca8dd0dc3
LeandroLSilva <leandro.l.silva@caixa.gov.br>
2015-07-23 19:40:04 -0300

    prévia dos testes da manada

--- test/test_manada.py
+++ test/test_manada.py
@@ -1,1 +1,25 @@
+import unittest
+from unittest.case import TestCase
 
+from os import path
+import sys
+lib_path = os.path.abspath(os.path.join('..'))
+sys.path.append(lib_path)
+
+import manada
+
+#class TestManada(TestCase):
+    #def test_manada(self):
+        #m = Manada()
+        #self.assertTrue(m)
+
+class TestAnimals(TestCase):
+    from manada import animals
+    from animals import Animal
+    
+    def test_animals(self):
+        a = Animal()
+        self.assertRaises(NotImplementedError, a.barulho())
+
+if __name__ == "__main__":
+    unittest.main()
